49563ede5df707e1ae4eaad51219dc4f7e835b7b
avinash <polisettyavinash2@gmail.com>
2024-02-02 23:42:52 +0530

    pipeline added

--- app.py
+++ app.py
@@ -1,5 +1,41 @@
+from flask import Flask,request,jsonify,render_template
+from src.pipeline.prediction_pipeline import PredictionPipeline,CustomData
+
 from src.logger import logging
 from src.exception import CustomException
 import sys
 
-logging.info("working")
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('form.html')
+
+    else:
+        data = CustomData(
+            age=float(request.form.get('age')),
+            sex=request.form.get('sex'),
+            bmi=float(request.form.get('bmi')),
+            children=float(request.form.get('children')),
+            smoker=request.form.get('smoker'),
+            region=request.form.get('region'),
+            
+        )
+        final_new_data = data.get_data_as_dataframe()
+        predict_pipeline = PredictionPipeline()
+        pred = predict_pipeline.predict(final_new_data)
+
+        results = round(pred[0], 2)
+
+        return render_template('results.html', final_result=results)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformationConfig,DataTransformation
 from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 from src.components.model_evaluation import ModelEvaluationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join("artifacts","train.csv")
@@ -48,19 +49,7 @@ class dataingestion:
 
 
         
-if __name__=="__main__":
-    obj=dataingestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    model_trainer=ModelTrainer()
-    y_test,predicted=model_trainer.initiate_model_train(train_arr,test_arr)
-    print('r2_score',"this is score")
 
-    model_evaluation=ModelEvaluationConfig()
-    rmse,mae,r2_score=model_evaluation.eval_metrics(predicted,y_test)
-    print(rmse,mae,r2_score,"in modelevaluation")
-
-    model_evaluation.log_into_mlflow()
+    
    
